9037a698ddc191a8b602f37be19fb804fb325bf0
Kevin Berry <kjb14d@my.fsu.edu>
2016-02-06 13:00:40 -0500

    adding groceries project

---
+++ groceries/www/js/app.js
@@ -0,0 +1,33 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('groceries', ['ionic', 'firebase'])
+
+.factory('Items', ['$firebaseArray', function($firebaseArray) {
+  var itemsRef = new Firebase('groceries-ist-app.firebaseIO.com/items');
+  return $firebaseArray(itemsRef);
+}])
+
+.controller('ListCtrl', function($scope, $ionicListDelegate, Items) {
+  
+  $scope.items = Items;
+
+  $scope.addItem = function() {
+    var name = prompt('What do you need to buy?');
+    if (name) {
+      $scope.items.$add({
+        'name' : name
+      });
+    }
+  };
+
+  $scope.purchaseItem = function(item) {
+    var itemRef = new Firebase('groceries-ist-app.firebaseIO.com/items' + item.$id);
+    itemRef.child('status').set('purchased');
+    $scope.item = item;
+    $scope.item['status'] = 'purchased';
+    $ionicListDelegate.closeOptionButtons();
+  };
+});
